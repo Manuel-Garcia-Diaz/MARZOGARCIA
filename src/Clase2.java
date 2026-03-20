39e868908bf09aa91fce535a530c4d0abd809d39
dam <dam@203P23.mshome.net>
2026-03-20 10:20:02 +0100

    Estamos en produccion

---
+++ src/Clase2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author dam
+ */
+public  class Clase2 {
+   
+    public static void visualizar(){
+        System.out.println("Estamos en produccion");
+        System.out.println("Manuel Garcia Diaz");
+    }
+
+}
